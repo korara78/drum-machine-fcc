d3d48712cf9d83de8f98c00b909808e53297f550
korara78 <79677336+korara78@users.noreply.github.com>
2021-06-12 23:25:18 -0700

    Commit

--- drum-machine-fcc/src/App.js
+++ drum-machine-fcc/src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
+import React from 'react';
+import React, { Component, } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const clips = [
+  {
+    key: 'Q',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    key: 'W',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    key: 'E',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    key: 'A',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    key: 'S',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    key: 'D',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    key: 'Z',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    key: 'X',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    key: 'C',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  },
+];
 
-export default App;
+const App = () => (
+  <div id="display" className="display">
+    <h1>Drum Machinery</h1>
+    {clips.map((sound, index) => (
+      <DrumPad text={sound.key} key={index} audio={sound.url} />
+    ))}
+  </div>
+);
+
+class DrumPad extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.audio = React.createRef();
+  }
+  
+  componentDidMount() {
+    this.audio.current.addEventListener('ended', (e) => {
+      const parent = e.target.parentNode;
+      parent.classList.remove('active');
+    });
+  }
+  
+  playSound = () => {
+    this.audio.current.play();
+    
+    const id = this.audio.current.id;
+    
+    const parent = this.audio.current.parentNode;
+    parent.classList.add('active');
+    
+    const display = parent.parentNode;
+    display.querySelector('h1').innerText = `${id} is in Effect`;
+  }
+  
+  render() {
+    const { text, audio } = this.props;
+    
+    return (
+      <div className="drum-pad" onClick={this.playSound} id={`drum-${text}`}>
+        {text}
+        <audio ref={this.audio} src={audio} className="clip" id={text} />
+      </div>
+    )
+  }
+}
+    
+document.addEventListener('keydown', (e) => {
+  const id = e.key.toUpperCase();
+  const audio = document.getElementById(id);
+  
+  if(audio) {
+    audio.currentTime = 0;
+    const parent = audio.parentNode;
+    parent.classList.add('active');
+    
+    const display = parent.parentNode;
+    display.querySelector('h1').innerText = `${id} is in Effect`;
+    audio.play();
+  }
+});
+
+ReactDOM.render(<App />, document.getElementById('drum-machine'));
