0be81ed2525fbc9d290722ce9c66d0bb5fe428b0
korara78 <79677336+korara78@users.noreply.github.com>
2021-08-07 22:10:14 -0700

    Update App.js
    Added comments

--- drum-machine-fcc/src/App.js
+++ drum-machine-fcc/src/App.js
@@ -41,6 +41,7 @@ const clips = [
   },
 ];
 
+//Apply the map method to const "clips" and assign its properties to become sound and index arguments
 const App = () => (
   <div id="display" className="display">
     <h1>Drum Machinery</h1>
@@ -56,7 +57,9 @@ class DrumPad extends React.Component {
     
     this.audio = React.createRef();
   }
-   //This componentDidMount() method is called after the component gets mounted on the DOM hence "did". Note: the render method is called before this executes//
+  /*componentDidMount() is invoked immediately after a component is mounted (inserted into the tree). Initialization that requires DOM nodes should go here. Also note: the render method is called before this executes*/ 
+  //child element <audio> listens for the 'ended' event then will set the const parent as the event.target.parentNode. 
+  //the classList API is used to remove class 'active'.
   componentDidMount() {
     this.audio.current.addEventListener('ended', (e) => {
       const parent = e.target.parentNode;
@@ -64,6 +67,12 @@ class DrumPad extends React.Component {
     });
   }
   
+  //Here playsound is an anonymous function that plays current audio clip based on key clicked.
+  //const "id" is defined as the current audio's id.
+  //const parent is defined as the <div> wrapping the child <audio> element.
+  //on parent, use the classList API to add the class 'active'.
+  //backticks indicate template literals. 
+  //${} is merely a placeholder to contain the javascript expression
   playSound = () => {
     this.audio.current.play();
     
@@ -72,6 +81,10 @@ class DrumPad extends React.Component {
     const parent = this.audio.current.parentNode;
     parent.classList.add('active');
     
+  //Display the key clicked in an H1 header.
+	//the querySelector() method returns the first element that matches ('h1').
+	//innertext property of the HTML Element interface represents the "rendered" text content of a node and its descendants.
+	//${expression} is a Template literal placeholder to insert javascript expression {id}. 
     const display = parent.parentNode;
     display.querySelector('h1').innerText = `${id} is in Effect`;
   }
@@ -79,6 +92,9 @@ class DrumPad extends React.Component {
   render() {
     const { text, audio } = this.props;
     
+  /*<div> element containing the className 'drum-pad' is the parent. When I click on a .drum-pad element, the audio clip contained in its child <audio> element should be triggered.*/
+	//onClick event is linked to the playSound function.
+	//advise React to associate the <audio> element ref with the 'audio' created in the constructor. 
     return (
       <div className="drum-pad" onClick={this.playSound} id={`drum-${text}`}>
         {text}
@@ -87,16 +103,28 @@ class DrumPad extends React.Component {
     )
   }
 }
-    
+  //Play key when user uses keyboard
+  //event listener attached to the document, listening for when the 'keydown' event is fired as a key is pressed.
+  //const "id" is defined by the event's key letter and set to uppercase.
+  //const "audio" is defined as the document Element ID to locate. Here its the ID of the key pressed.  
 document.addEventListener('keydown', (e) => {
   const id = e.key.toUpperCase();
   const audio = document.getElementById(id);
   
+  
+  //if audio plays, set playback position at 0 second mark.
+  //const parent defined as the parent to the <audio> element which is <div> element containing child <audio> element.
+  //on parent, use the classList API to add the class 'active'.
   if(audio) {
     audio.currentTime = 0;
     const parent = audio.parentNode;
     parent.classList.add('active');
     
+   //Display the key pressed in an H1 header.
+	 //const display defined as parentNode.
+   //The querySelector() method returns the first element that matches ('h1').
+   //innertext property of the HTML Element interface represents the "rendered" text content of a node and its descendants.
+   //${expression} is a Template literal placeholder to insert javascript expression {id}.   
     const display = parent.parentNode;
     display.querySelector('h1').innerText = `${id} is in Effect`;
     audio.play();
